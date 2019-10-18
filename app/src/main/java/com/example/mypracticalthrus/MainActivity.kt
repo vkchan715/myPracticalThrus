f5cfdcba31063e12a4ee75d341964de22db31604
vkchan715 <boyzkeit@gmail.com>
2019-10-18 22:39:00 +0800

    18/10/2019 10.38pm

--- app/src/main/java/com/example/mypracticalthrus/MainActivity.kt
+++ app/src/main/java/com/example/mypracticalthrus/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.mypracticalthrus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+
+        val rollButton2: Button = findViewById(R.id.roll_button2)
+        rollButton2.setOnClickListener{ countDice()}
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+
+    }
+
+    private fun countDice(){
+        Toast.makeText(this, "Dice added",
+            Toast.LENGTH_SHORT).show()
+        val resulttext: TextView = findViewById(R.id.result_text)
+        val number = "Hello"
+
+        resulttext.text = number
+
     }
 }
